93b516722e9400ff5c95223d74625616f621d754
Adametsofficial <av.styledev@gmail.com>
2020-05-03 03:33:53 +0600

    Version --Part 2

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 <h2>
 Create a custom select on js without js-package 
 </h2>
+
+<ul>
+  <li>USING CLASS IN A TWO PART</li>
+</ul>
--- src/select.js
+++ src/select.js
@@ -1,75 +1,125 @@
-const select = document.querySelector(".select");
-
-const selectContainer = document.createElement("div");
-selectContainer.classList.add("selectContainer");
-select.appendChild(selectContainer);
-
-const triangle = document.createElement("span");
-triangle.classList.add("triangle");
-selectContainer.appendChild(triangle);
-
-const mnemoText = document.createElement("span");
-mnemoText.innerText = "Choose option";
-mnemoText.classList.add("mnemoText");
-selectContainer.appendChild(mnemoText);
-
-const selectBox = document.createElement("div");
-selectBox.classList.add("selectBox");
-
-let options = [
-  { name: "React" },
-  { name: "Angular" },
-  { name: "Vue" },
-  { name: "Ionic" },
-  { name: "Abs" },
-  { name: "Component" },
-  { name: "Test1" },
-  { name: "test2" },
-  { name: "test3" },
-  { name: "test4" },
-];
-
-let chooseOption = null;
-
-select.addEventListener("click", (e) => {
-  triangle.classList.toggle("triangle-rotate");
-  if (document.querySelector(".selectBox")) {
-    document.querySelector(".selectBox").classList.add("closeSelectBox");
+class Select {
+  constructor(selectArrayWithObject = [], firstText) {
+    this.selectArrayWithObject = selectArrayWithObject;
+    this.firstText = firstText;
+    this.render();
   }
 
-  if (!document.querySelector(".selectBox")) {
-    select.appendChild(selectBox);
+  select =  document.createElement("div");
 
-    options.map((i) => {
-      const optionEl = document.createElement("div");
-      optionEl.classList.add("option");
-      optionEl.innerText += i.name;
-      document.querySelector(".selectBox").append(optionEl);
+  value = null;
 
-      optionEl.addEventListener("click", (e) => {
-        mnemoText.innerText = e.target.textContent;
-        chooseOption = e.target.textContent;
+  documentCreateElement(element = "div", classNames, append, value) {
+    let elem = document.createElement(element);
 
-        if (e.isTrusted) {
-          e.target.classList.toggle("s");
-        }
+    if (!classNames || classNames === "") {
+      console.warn("ClassNames was not specified");
+      return;
+    }
 
-        render();
+    if (value) {
+      elem.textContent = value;
+    }
+
+    elem.classList.add(classNames);
+    append && append.append(elem);
+    return elem;
+  }
+
+  checkClassForToggleSelectBox = (selectBox) => {
+    if (!selectBox.classList.contains("openedSelectBox")) {
+      selectBox.classList.remove("closeSelectBox");
+      selectBox.classList.add("openedSelectBox");
+    } else {
+      selectBox.classList.remove("openedSelectBox");
+      selectBox.classList.add("closeSelectBox");
+    }
+  };
+
+  UI() {
+    return {
+      triangle: this.documentCreateElement("div", "triangle"),
+      selectBox: this.documentCreateElement("div", "selectBox", this.select),
+      textView: this.documentCreateElement("div", "textView"),
+    };
+  }
+
+  selectBar() {
+    const { triangle, selectBox, textView } = this.UI();
+    const selectBar = this.documentCreateElement(
+      "div",
+      "selectContainer",
+      this.select
+    );
+    selectBar.appendChild(triangle);
+    selectBar.appendChild(textView);
+    textView.textContent = this.firstText || "Choose";
+
+    selectBar.addEventListener("click", () => {
+      triangle.classList.toggle("triangle-rotate");
+      this.checkClassForToggleSelectBox(selectBox);
+
+      selectBox.innerHTML = "";
+      this.selectArrayWithObject.map((option) => {
+        let opt = this.documentCreateElement(
+          "div",
+          "option",
+          selectBox,
+          option.name
+        );
+
+        opt.addEventListener("click", (e) => {
+          triangle.classList.toggle("triangle-rotate");
+          this.checkClassForToggleSelectBox(selectBox);
+          let text = e.target.textContent;
+
+          if (e.target.textContent.length > 25) {
+            textView.textContent = text.substring(0, 20) + "...";
+          } else {
+            textView.textContent = text;
+          }
+
+          this.value = text;
+          return this.innerValue();
+        });
       });
     });
-  } else {
-    setTimeout(() => {
-      document.querySelector(".selectBox").classList.remove("closeSelectBox");
-      select.removeChild(selectBox);
-      selectBox.innerHTML = "";
-    }, 200);
+
+    return selectBar;
   }
-});
 
-const render = () => {
-  console.log(chooseOption);
-
+  innerValue() {
   // USING CHOOSE OPTION IN A RENDER FUNC.
-};
 
-render();
+    return this.value;
+  }
+
+  render() {
+      this.select.id = "select";
+      document.body.append(this.select);
+    
+    if (!document.querySelector("selectContainer")) {
+      this.selectBar();
+    }
+  }
+}
+
+const sel = new Select([
+  { name: "React" },
+  { name: "React2" },
+  { name: "React3" },
+  { name: "React4" },
+  { name: "React5" },
+  { name: "React6" },
+  { name: "React7" },
+  { name: "React8" },
+  { name: "React9" },
+  { name: "React0" },
+  { name: "React12" },
+  { name: "React34" },
+  { name: "React45" },
+  { name: "React56asdasdasdasdasdasdasdasdasdasdasdasdasdasd" },
+]);
+
+console.log(sel.innerValue())
+
